80db388de9ec06b30f55ea4633202fa5faad3a90
原島輝 <>
2020-12-29 15:55:58 +0900

    分割キーボード表示とHMKボタン

---
+++ Assets/Scripts/Practice/KeyObserver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using DG.Tweening;
+
+public class KeyObserver : MonoBehaviour
+{
+    [SerializeField]
+    private List<Image> keyImages;
+
+    [SerializeField]
+    private float duration = 0f;
+
+    private void Update()
+    {
+        if (Input.anyKeyDown) {
+            var key = Input.inputString;
+            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(code))
+                {
+                    keyImages.Where(k => k.name.Equals(key.ToUpper())).FirstOrDefault().DOColor(Color.red, duration);
+                    keyImages.Where(k => k.name.Equals(key.ToUpper())).FirstOrDefault().DOColor(Color.white, duration).SetDelay(duration);
+                }
+            }
+        }
+
+    }
+}
--- Assets/Scripts/StartUI.cs
+++ Assets/Scripts/StartUI.cs
@@ -10,15 +10,23 @@ public class StartUI : MonoBehaviour
     private Subject<Unit> onLaser = new Subject<Unit>();
     public IObservable<Unit> OnLaser => onLaser;
 
+    private bool isShowKeyboard = true;
+
     public int CountTime = 5;
     public Text CountDownText;
     public Button StartButton;
     public Button LaserModeButton;
+    public Button HmkModeButton;
+    public GameObject canvasKeyboard;
 
     public void ExecuteTask()
     {
         StartButton.gameObject.SetActive(false);
         LaserModeButton.gameObject.SetActive(false);
+        HmkModeButton.gameObject.SetActive(false);
+        canvasKeyboard.SetActive(true);
+
+        canvasKeyboard.SetActive(isShowKeyboard);
 
         Observable
             .Timer(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1))
@@ -40,5 +48,10 @@ public class StartUI : MonoBehaviour
     public void OnLaserMode()
     {
         onLaser.OnNext(Unit.Default);
+    }
+
+    public void OnHmkMode()
+    {
+        isShowKeyboard = false;
     }
 }
